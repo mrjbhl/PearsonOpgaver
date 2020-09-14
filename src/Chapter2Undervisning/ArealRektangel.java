0ded1ad09e897494fa95631051631d320813553a
mrjbhl <70877369+mrjbhl@users.noreply.github.com>
2020-09-14 10:32:32 +0200

    MilesToKm

--- src/Chapter2Undervisning/ArealRektangel.java
+++ src/Chapter2Undervisning/ArealRektangel.java
@@ -4,11 +4,12 @@ public class ArealRektangel {
     public static void main(String[] args) {
         System.out.println("Beregner areal af rektangel med metoder");
         System.out.println("Arealet for to sider af 2 og 6 er: " + beregnAreal(2, 6));
+        System.out.println("Arealet for to sider af 5 og 6 er: " + beregnAreal(5, 6));
     }
 
-
+    // metode til beregning af areal
     public static double beregnAreal(double sideA, double sideB) {
-        double areal = sideA + sideB;
+        double areal = sideA * sideB;
         return areal;
 
     }
