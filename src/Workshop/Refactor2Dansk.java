a2a3a22e682d49e547295c69ec86f1cedab40003
mrjbhl <70877369+mrjbhl@users.noreply.github.com>
2020-09-17 10:36:00 +0200

    Workshop Task Refactor2 - Dansk Færdig

--- src/Workshop/Refactor2Dansk.java
+++ src/Workshop/Refactor2Dansk.java
@@ -3,15 +3,15 @@ import java.util.Scanner;
 
 public class Refactor2Dansk {
     public static void main(String[] args) {
-        ShowCurrentTime();
         VisTid();
+        UdregnTid();
         FahTilCel();
-        ComputeLoan();
+        UdregnLaan();
         ComputeChange();
 
     }
 
-    public static void VisTid() {
+    public static void UdregnTid() {
         Scanner input = new Scanner(System.in);
         // Spørg efter input
         System.out.print("Skriv et heltal sekunder ");
@@ -34,100 +34,87 @@ public class Refactor2Dansk {
         System.out.println("Fahrenheit " + fahrenheit + " er lig med " +
                 celsius + " Celsius");
     }
-    public static void ShowCurrentTime() {
-        // Obtain the total milliseconds since midnight, Jan 1, 1970
-        long totalMilliseconds = System.currentTimeMillis();
+    public static void VisTid() {
+        // MilliSekunder siden midnat
+        long totalMillisekunder = System.currentTimeMillis();
 
-        // Obtain the total seconds since midnight, Jan 1, 1970
-        long totalSeconds = totalMilliseconds / 1000;
+        // Sekunder siden midnat
+        long totalSekunder = totalMillisekunder / 1000;
 
-        // Compute the current second in the minute in the hour
-        long currentSecond = totalSeconds % 60;
+        // Udregn nuværende sekunder i minuttet i timen
+        long nuSekunder = totalSekunder % 60;
 
-        // Obtain the total minutes
-        long totalMinutes = totalSeconds / 60;
+        // Total minutter
+        long totalMinutter = totalSekunder / 60;
 
-        // Compute the current minute in the hour
-        long currentMinute = totalMinutes % 60;
+        // udregn nuværende minutter i timen
+        long nuMinutter = totalMinutter % 60;
 
-        // Obtain the total hours
-        long totalHours = totalMinutes / 60;
+        // total timer
+        long totalTimer = totalMinutter / 60;
 
-        // Compute the current hour
-        long currentHour = totalHours % 24;
+        // udregn nuværende timer
+        long nuTimer = totalTimer % 24;
 
-        // Display results
-        System.out.println("Current time is " + currentHour + ":"
-                + currentMinute + ":" + currentSecond + " GMT");
+        // Vis resultat
+        System.out.println("Klokken er " + nuTimer + ":"
+                + nuMinutter + ":" + nuSekunder + " GMT");
     }
-    public static void ComputeLoan() {
-        // Create a Scanner
+    public static void UdregnLaan() {
+        // lav en Scanner
         Scanner input = new Scanner(System.in);
 
-        // Enter yearly interest rate
-        System.out.print("Enter yearly interest rate, for example 8.25: ");
-        double annualInterestRate = input.nextDouble();
+        // Årlig rente
+        System.out.print("Skriv årlig rente: ");
+        double aarligRente = input.nextDouble();
 
-        // Obtain monthly interest rate
-        double monthlyInterestRate = annualInterestRate / 1200;
+        // Månedlig Rente
+        double maanedligRente = aarligRente / 1200;
 
-        // Enter number of years
+        // Antal år
         System.out.print(
-                "Enter number of years as an integer, for example 5: ");
-        int numberOfYears = input.nextInt();
+                "Skriv antal år, som heltal: ");
+        int antalAar = input.nextInt();
 
-        // Enter loan amount
-        System.out.print("Enter loan amount, for example 120000.95: ");
-        double loanAmount = input.nextDouble();
+        // Skriv lån størrelse
+        System.out.print("Skrive lånets størrelse: ");
+        double laanStoerelse = input.nextDouble();
 
-        // Calculate payment
-        double monthlyPayment = loanAmount * monthlyInterestRate / (1
-                - 1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12));
-        double totalPayment = monthlyPayment * numberOfYears * 12;
+        // Udregn afbetaling
+        double maanedligAfbetaling = laanStoerelse * maanedligRente / (1
+                - 1 / Math.pow(1 + maanedligRente, antalAar * 12));
+        double totalAfbetaling = maanedligAfbetaling * antalAar * 12;
 
-        // Display results
-        System.out.println("The monthly payment is $" +
-                (int)(monthlyPayment * 100) / 100.0);
-        System.out.println("The total payment is $" +
-                (int)(totalPayment * 100) / 100.0);
+        // Vis resultat
+        System.out.println("Månedlig afbetaling er KR" +
+                (int)(maanedligAfbetaling * 100) / 100.0);
+        System.out.println("Den totale tilbagebetaling er KR" +
+                (int)(totalAfbetaling * 100) / 100.0);
     }
     public static void ComputeChange() {
-        // Create a Scanner
+        // Lav Scanner
         Scanner input = new Scanner(System.in);
 
-        // Receive the amount
+        // Få beløb
         System.out.print(
-                "Enter an amount in double, for example 11.56: ");
-        double amount = input.nextDouble();
+                "Skriv beløb: ");
+        double beloeb = input.nextDouble();
 
-        int remainingAmount = (int)(amount * 100);
+        int restBeloeb = (int)(beloeb * 100);
 
-        // Find the number of one dollars
-        int numberOfOneDollars = remainingAmount / 100;
-        remainingAmount = remainingAmount % 100;
+        // find antal kroner
+        int antalKroner = restBeloeb / 100;
+        restBeloeb = restBeloeb % 100;
 
-        // Find the number of quarters in the remaining amount
-        int numberOfQuarters = remainingAmount / 25;
-        remainingAmount = remainingAmount % 25;
+        // Find antal øre
+        int antalOere = restBeloeb;
 
-        // Find the number of dimes in the remaining amount
-        int numberOfDimes = remainingAmount / 10;
-        remainingAmount = remainingAmount % 10;
 
-        // Find the number of nickels in the remaining amount
-        int numberOfNickels = remainingAmount / 5;
-        remainingAmount = remainingAmount % 5;
+        // vis resultat
+        System.out.println("Dit beløb " + beloeb + " består af");
+        System.out.println("    " + antalKroner + " Kroner");
+        System.out.println("    " + antalOere + " Øre ");
 
-        // Find the number of pennies in the remaining amount
-        int numberOfPennies = remainingAmount;
-
-        // Display results
-        System.out.println("Your amount " + amount + " consists of");
-        System.out.println("    " + numberOfOneDollars + " dollars");
-        System.out.println("    " + numberOfQuarters + " quarters ");
-        System.out.println("    " + numberOfDimes + " dimes");
-        System.out.println("    " + numberOfNickels + " nickels");
-        System.out.println("    " + numberOfPennies + " pennies");
     }
 }
 
