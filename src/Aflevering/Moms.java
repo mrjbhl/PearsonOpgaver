e89fa83328285e5f29a059c4ebd4e08cb0eef32d
mrjbhl <70877369+mrjbhl@users.noreply.github.com>
2020-09-17 09:11:28 +0200

    Refactor 3 + 4 Ny metode udregner tysk moms PlusMoms hedder nu plusDanskMoms()

--- src/Aflevering/Moms.java
+++ src/Aflevering/Moms.java
@@ -7,15 +7,24 @@ public class Moms {
     static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        plusMoms();
+        plusDanskMoms();
+        plusTyskMoms();
     }
 
-    static String plusMoms() {
-        System.out.print("Skriv prisen: ");
+    static String plusDanskMoms() {
+        System.out.print("Skriv prisen (Dansk): ");
         double pris = input.nextDouble();
 
         double moms = pris * 0.25;
-        System.out.println("Momsen er " + (int)(moms * 100) / 100.0);
-        return "Moms er: " + moms;
+        System.out.println("Momsen for Danmark er " + (int)(moms * 100) / 100.0);
+        return "";
+    }
+    static String plusTyskMoms() {
+        System.out.print("Skriv prisen (Tysk): ");
+        double pris = input.nextDouble();
+
+        double moms = pris * 0.16;
+        System.out.println("Momsen for Tyskland er " + (int)(moms * 100) / 100.0);
+        return "";
     }
 }
