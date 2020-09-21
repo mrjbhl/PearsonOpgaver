ffceff2b971d043cba23020cc67d4e6c25ce52ad
mrjbhl <70877369+mrjbhl@users.noreply.github.com>
2020-09-21 10:34:58 +0200

    Undervisning c3

---
+++ src/Cahpter3Undervisning/Eksempel.java
@@ -0,0 +1,8 @@
+package Cahpter3Undervisning;
+
+public class Eksempel {
+    static void metode() {
+        System.out.println("Hej");
+        
+    }
+}
---
+++ src/Cahpter3Undervisning/Selections.java
@@ -0,0 +1,41 @@
+package Cahpter3Undervisning;
+
+import java.awt.*;
+import java.util.Scanner;
+
+public class Selections {
+    public static void main(String[] args) {
+        Eksempel.metode();
+        double hour = 0;
+        int minut = 57;
+
+        //if
+        if (hour >= 9) {
+            if (hour < 15) {
+                System.out.println("Du er i skole");
+            } else {
+                System.out.println("Du har fri");
+            }
+        } else {
+            System.out.println("Du har fri");
+        }
+        if (hour == 0 && minut < 60) System.out.println("Det er midnat bro");
+
+        // switch
+
+        String color = "red";
+        int tal = 0;
+
+        switch (color) {
+            case "red":
+                System.out.println("Sport");
+                break;
+            case "green":
+                System.out.println("Offroad");
+                break;
+            default:
+                System.out.println("Random Bil");
+        }
+
+    }
+}
